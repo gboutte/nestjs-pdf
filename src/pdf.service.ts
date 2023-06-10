d147520a7c7e25caf6ecf1a6acfde799c039d403
gboutte <btte.gregory@gmail.com>
2023-06-11 00:35:01 +0200

    opti

--- src/pdf.service.ts
+++ src/pdf.service.ts
@@ -32,9 +32,50 @@ export class PdfService {
         if (this.options.headless !== undefined) {
             headless = this.options.headless;
         }
+
+        //src: https://www.bannerbear.com/blog/ways-to-speed-up-puppeteer-screenshots/
+        const minimal_args = [
+            '--autoplay-policy=user-gesture-required',
+            '--disable-background-networking',
+            '--disable-background-timer-throttling',
+            '--disable-backgrounding-occluded-windows',
+            '--disable-breakpad',
+            '--disable-client-side-phishing-detection',
+            '--disable-component-update',
+            '--disable-default-apps',
+            '--disable-dev-shm-usage',
+            '--disable-domain-reliability',
+            '--disable-extensions',
+            '--disable-features=AudioServiceOutOfProcess',
+            '--disable-hang-monitor',
+            '--disable-ipc-flooding-protection',
+            '--disable-notifications',
+            '--disable-offer-store-unmasked-wallet-cards',
+            '--disable-popup-blocking',
+            '--disable-print-preview',
+            '--disable-prompt-on-repost',
+            '--disable-renderer-backgrounding',
+            '--disable-setuid-sandbox',
+            '--disable-speech-api',
+            '--disable-sync',
+            '--hide-scrollbars',
+            '--ignore-gpu-blacklist',
+            '--metrics-recording-only',
+            '--mute-audio',
+            '--no-default-browser-check',
+            '--no-first-run',
+            '--no-pings',
+            '--no-zygote',
+            '--password-store=basic',
+            '--use-gl=swiftshader',
+            '--use-mock-keychain',
+            "--disabled-setupid-sandbox"
+        ];
+
         const browser = await puppeteer.launch({
             headless: headless,
             executablePath: revisionInfo.executablePath,
+            args: minimal_args
         });
 
         const page = await browser.newPage();
