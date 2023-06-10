b038ce46ec40da84c937ce83dea66a827c650682
gboutte <btte.gregory@gmail.com>
2023-06-10 11:34:22 +0200

    opti chromium download

--- src/pdf.service.ts
+++ src/pdf.service.ts
@@ -1,51 +1,62 @@
-import { HandlebarsService } from "@gboutte/nestjs-hbs";
-import { Inject, Injectable } from "@nestjs/common";
+import {HandlebarsService} from "@gboutte/nestjs-hbs";
+import {Inject, Injectable} from "@nestjs/common";
 
 import * as puppeteer from "puppeteer";
-import { PdfParameters } from "./pdf-parameters.interface";
+import {PdfParameters} from "./pdf-parameters.interface";
 
 @Injectable()
 export class PdfService {
-  constructor(
-    private readonly hbsService: HandlebarsService,
-    @Inject("PDF_PARAMETERS") private options: PdfParameters
-  ) {}
-
-  async generatePdfFromHtml(html: string): Promise<Buffer> {
-    let chromiumRevision = '1095492';
-    if (this.options.chromiumRevision !== undefined) {
-      chromiumRevision = this.options.chromiumRevision;
+    constructor(
+        private readonly hbsService: HandlebarsService,
+        @Inject("PDF_PARAMETERS") private options: PdfParameters
+    ) {
     }
-    let revisionInfo = await puppeteer.createBrowserFetcher().download(chromiumRevision)
-    const browser = await puppeteer.launch({
-      headless: "new",
-      executablePath: revisionInfo.executablePath,
-    });
 
-    const page = await browser.newPage();
+    async generatePdfFromHtml(html: string): Promise<Buffer> {
 
-    await page.setContent(html, {
-      waitUntil: "domcontentloaded",
-    });
-    let pdfopt;
-    if (this.options.pdfOptions === undefined) {
-      pdfopt = {
-        format: "A4",
-      };
-    } else {
-      pdfopt = this.options.pdfOptions;
+        const browserFetcher = puppeteer.createBrowserFetcher();
+        const revisions = browserFetcher.localRevisions();
+
+        let chromiumRevision = '1095492';
+        if (this.options.chromiumRevision !== undefined) {
+            chromiumRevision = this.options.chromiumRevision;
+        }
+        let revisionInfo;
+        if (!revisions.includes(chromiumRevision)) {
+            revisionInfo = await browserFetcher.download(chromiumRevision)
+        } else {
+            revisionInfo = browserFetcher.revisionInfo(chromiumRevision);
+        }
+        const browser = await puppeteer.launch({
+            headless: "new",
+            executablePath: revisionInfo.executablePath,
+        });
+
+        const page = await browser.newPage();
+
+        await page.setContent(html, {
+            waitUntil: "domcontentloaded",
+        });
+        let pdfopt;
+        if (this.options.pdfOptions === undefined) {
+            pdfopt = {
+                format: "A4",
+            };
+        } else {
+            pdfopt = this.options.pdfOptions;
+        }
+        // create a pdf buffer
+        const pdfBuffer = await page.pdf(pdfopt);
+        return pdfBuffer;
     }
-    // create a pdf buffer
-    const pdfBuffer = await page.pdf(pdfopt);
-    return pdfBuffer;
-  }
 
-  async generatePdfFromTemplateString(template: string, parameters: any = {}) {
-    const html = this.hbsService.render(template, parameters);
-    return this.generatePdfFromHtml(html);
-  }
-  async generatePdfFromTemplateFile(file: string, parameters: any = {}) {
-    const html = this.hbsService.renderFile(file, parameters);
-    return this.generatePdfFromHtml(html);
-  }
+    async generatePdfFromTemplateString(template: string, parameters: any = {}) {
+        const html = this.hbsService.render(template, parameters);
+        return this.generatePdfFromHtml(html);
+    }
+
+    async generatePdfFromTemplateFile(file: string, parameters: any = {}) {
+        const html = this.hbsService.renderFile(file, parameters);
+        return this.generatePdfFromHtml(html);
+    }
 }
